eb83951b9f74f63218b30dcc75d70522eccf09c5
Akashkarmokar <akashcsemu@gmail.com>
2020-10-07 07:48:20 +0600

    Added form class and remove input tag by button

--- pastebin/forms.py
+++ pastebin/forms.py
@@ -6,15 +6,55 @@ from . models import Pastebindb # here i import my database table
 
 poster_type_choice = (
     ('none','None'),
+    ('markup','Atom'),
     ('c','C'),
     ('cpp','C++'),
-    ('py','Python'),
+    ('css','CSS'),
+    ('go','Go'),
+    ('git','Git'),
+    ('markup','HTML'),
+    ('js','JavaScript'),
     ('java','Java'),
+    ('javadoc','JavaDoc'),
+    ('javadoclike','JavaDoc-like'),
+    ('javastacktrace','Java stack trace'),
+    ('jq','JQ'),
+    ('jsdoc','JSDoc'),
+    ('js-extras','JS Extras'),
+    ('json','JSON'),
+    ('json5','JSON5'),
+    ('jsonp','JSONP'),
+    ('kotlin','Kotlin'),
+    ('kt','Kotlin(kt)'),
+    ('kts','Kotlin(kts)'),
+    ('livescript','LiveScript'),
+    ('markup','Markup'),
+    ('markup','Mathml'),
+    ('mongodb','MongoDB'),
+    ('matlab','MATLAB'),
+    ('py','Python'),
+    ('powershell','PowerShell'),
+    ('processing','Processing'),
+    ('pug','Pug'),
+    ('perl','Perl'),
+    ('php','PHP'),
+    ('phpdoc','PHPDoc'),
+    ('r','R'),
+    ('markup','SVG'),
+    ('markup','SSML'),
+    ('scheme','Scheme'),
+    ('sql','SQL'),
+    ('swift','Swift'),
+    ('vim','vim'),
+    ('markup','XML'), 
 )
 
 
 class pasteFm(forms.ModelForm):
-    poster_type = forms.ChoiceField(choices=poster_type_choice)
+    poster_type = forms.ChoiceField(
+        choices=poster_type_choice,
+        help_text=" Select one to syntax highlight otherwise code will be considered as plain-text.",
+    )
     class Meta:
         model = Pastebindb
         fields = ['poster_name','poster_type','poster']
@@ -24,7 +64,10 @@ class pasteFm(forms.ModelForm):
             'poster':'Content ',
         }
         widgets = {
+            'poster_name':forms.TextInput(
+                attrs={'class':'form-group'}
+            ),
             'poster':forms.Textarea(
-                attrs={'rows':'15','cols':'150'}
+                attrs={'class':'form-control','rows':'15','cols':'150'}
             ),
         }
