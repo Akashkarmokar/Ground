ccda7d62c06ec39e8f6078a00eae646fed36dbd2
Akashkarmokar <akashcsemu@gmail.com>
2020-10-07 12:04:52 +0600

    Create User's SignUpForm and LoginForm

--- Ground/urls.py
+++ Ground/urls.py
@@ -22,4 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name="home_url"),
     path('pastebin/',include('pastebin.urls')),
+    path('signup/',views.signup,name='signup'),
+    path('login/',views.user_login,name='login'),
+    path('profile/',views.profile,name='profile'),
+    path('logout/',views.user_logout,name='logout'),
 ]
