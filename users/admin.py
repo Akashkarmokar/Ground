0ebbfd1fd45ed33407b457735a8a497a5e64b213
Atikul Islam Atik <islam.atik15@gmail.com>
2020-10-28 00:14:24 +0600

    admin dashboard

--- users/admin.py
+++ users/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from .models import Profile,Relationship
 
 # Register your models here.
-# @admin.register(Profile)
-# class ProfileAdmin(admin.ModelAdmin):
-#     list_display = ['first_name','last_name','user','bio','email','country','avater','friends','slug','updated','created']
-admin.site.register(Profile)
+
+class Porfileadmin(admin.ModelAdmin):
+
+    list_display = ('first_name' , 'last_name' , 'user' , 'bio' ,'profile_photo')
+    list_filter = ['user','first_name']
+    list_per_page = 5
+    list_max_show_all = 50
+    
+admin.site.register(Profile,Porfileadmin)
 admin.site.register(Relationship)
