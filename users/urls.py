4b12a7d178769090aa47d22ce2146656b2728b35
Akashkarmokar <akashcsemu@gmail.com>
2020-10-28 21:58:46 +0600

    init

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,4 @@ urlpatterns =[
     path('signup/',views.signup,name='signup'),
     path('login/',views.user_login,name='login'),
     path('logout/',views.user_logout,name='logout'),
-    path('myInvites/',views.invites_received_view,name='invitations'),
 ]
