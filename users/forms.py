9eb97301e70f0ee76845c69f9cacee64c7d10805
Akashkarmokar <akashcsemu@gmail.com>
2020-11-03 01:41:06 +0600

    valid username condition added with regex validation

--- users/forms.py
+++ users/forms.py
@@ -3,12 +3,22 @@ from django.contrib.auth.forms import UserCreationForm,AuthenticationForm,Userna
 from django.contrib.auth.models import User
 from django.utils.translation import gettext,gettext_lazy as _
 from .models import Profile
+from django.core.validators import RegexValidator
 
 
 #Custom user SignUpForm
+# ^[A-Za-z]\\w{5, 29}$
+# r'^[0-9a-zA-Z]*$'
+
+
 class SignUpForm(UserCreationForm):
     password1 = forms.CharField(label='Password',widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Password'}))
     password2 = forms.CharField(label='Confirm Password',widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Confirm Password'}))
+    username = forms.CharField(label='', validators=[RegexValidator('^[a-zA-Z]+([_ -]?[a-zA-Z0-9])*$', message="Username should be follow characters")],
+                required=True,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Username',}))
+    first_name = forms.CharField(label='',required=True,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'First Name'}))
+    last_name = forms.CharField(label='',required=True,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Last Name'}))
+    email = forms.EmailField(label='',required=True,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Email'}))
     class Meta:
         model = User
         fields = ['username','first_name','last_name','email']
